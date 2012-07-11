aacaf9112dd6d7a3b2d704431728d350fd388303
Bermon Painter <hello@bermonpainter.com>
2012-07-11 10:53:34 -0500

    Cleaned up the mixins, fixed compile errors and got Susy up and running with the new version.

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem "sass", "3.2.0.alpha.261"
+gem "compass", "0.13.alpha.0"
+gem "susy", "1.0.rc.2"
--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
 # Require any additional compass plugins here.
+require "susy"
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -15,10 +16,3 @@ javascripts_dir = "javascripts"
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
